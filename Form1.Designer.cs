771a5ee1242aa5ae4b3084d02f44cbe7205c1a07
Van Tin <tindragonmanilat@gmail.com>
2025-12-05 20:29:32 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BasicChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpConnection = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.lblIp = new System.Windows.Forms.Label();
+            this.rdClient = new System.Windows.Forms.RadioButton();
+            this.rdServer = new System.Windows.Forms.RadioButton();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.grpConnection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpConnection
+            // 
+            this.grpConnection.Controls.Add(this.lblStatus);
+            this.grpConnection.Controls.Add(this.btnStart);
+            this.grpConnection.Controls.Add(this.txtPort);
+            this.grpConnection.Controls.Add(this.lblPort);
+            this.grpConnection.Controls.Add(this.txtIp);
+            this.grpConnection.Controls.Add(this.lblIp);
+            this.grpConnection.Controls.Add(this.rdClient);
+            this.grpConnection.Controls.Add(this.rdServer);
+            this.grpConnection.Location = new System.Drawing.Point(16, 15);
+            this.grpConnection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpConnection.Name = "grpConnection";
+            this.grpConnection.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpConnection.Size = new System.Drawing.Size(611, 121);
+            this.grpConnection.TabIndex = 0;
+            this.grpConnection.TabStop = false;
+            this.grpConnection.Text = "Kết nối";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Green;
+            this.lblStatus.Location = new System.Drawing.Point(21, 85);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(89, 16);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Chưa kết nối...";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(473, 27);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 68);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(340, 65);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(105, 22);
+            this.txtPort.TabIndex = 5;
+            this.txtPort.Text = "9000";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(271, 69);
+            this.lblPort.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(37, 16);
+            this.lblPort.TabIndex = 4;
+            this.lblPort.Text = "Port: ";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(340, 33);
+            this.txtIp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(105, 22);
+            this.txtIp.TabIndex = 3;
+            this.txtIp.Text = "127.0.0.1";
+            // 
+            // lblIp
+            // 
+            this.lblIp.AutoSize = true;
+            this.lblIp.Location = new System.Drawing.Point(271, 37);
+            this.lblIp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIp.Name = "lblIp";
+            this.lblIp.Size = new System.Drawing.Size(53, 16);
+            this.lblIp.TabIndex = 2;
+            this.lblIp.Text = "Server: ";
+            // 
+            // rdClient
+            // 
+            this.rdClient.AutoSize = true;
+            this.rdClient.Location = new System.Drawing.Point(129, 33);
+            this.rdClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdClient.Name = "rdClient";
+            this.rdClient.Size = new System.Drawing.Size(61, 20);
+            this.rdClient.TabIndex = 1;
+            this.rdClient.Text = "Client";
+            this.rdClient.UseVisualStyleBackColor = true;
+            // 
+            // rdServer
+            // 
+            this.rdServer.AutoSize = true;
+            this.rdServer.Checked = true;
+            this.rdServer.Location = new System.Drawing.Point(25, 33);
+            this.rdServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdServer.Name = "rdServer";
+            this.rdServer.Size = new System.Drawing.Size(68, 20);
+            this.rdServer.TabIndex = 0;
+            this.rdServer.TabStop = true;
+            this.rdServer.Text = "Server";
+            this.rdServer.UseVisualStyleBackColor = true;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(16, 143);
+            this.txtChat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtChat.Size = new System.Drawing.Size(609, 262);
+            this.txtChat.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(16, 422);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(488, 22);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(513, 420);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(113, 28);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Gửi";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 469);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.grpConnection);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LanChat - Siêu cơ bản";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.grpConnection.ResumeLayout(false);
+            this.grpConnection.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpConnection;
+        private System.Windows.Forms.RadioButton rdClient;
+        private System.Windows.Forms.RadioButton rdServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label lblIp;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace BasicChat
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener _listener;
+        private TcpClient _client;
+        private NetworkStream _stream;
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+        private readonly object _streamLock = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+            rdServer.Checked = true;
+            txtIp.Text = "127.0.0.1";
+            txtPort.Text = "9000";
+            UpdateUIForMode();
+            
+            // Thêm event handler để ẩn/hiện txtIP khi đổi mode
+            rdServer.CheckedChanged += (s, e) => UpdateUIForMode();
+            rdClient.CheckedChanged += (s, e) => UpdateUIForMode();
+        }
+
+        private void UpdateUIForMode()
+        {
+            // Server không cần IP, Client cần IP của server
+            bool isClient = rdClient.Checked;
+            txtIp.Visible = isClient;
+            lblIp.Visible = isClient;
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtPort.Text, out int port))
+            {
+                MessageBox.Show("Port không hợp lệ", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            btnStart.Enabled = false;
+            if (rdServer.Checked)
+            {
+                StartServer(port);
+            }
+            else
+            {
+                await ConnectAsClient(txtIp.Text.Trim(), port);
+            }
+        }
+
+        private void StartServer(int port)
+        {
+            try
+            {
+                _listener = new TcpListener(IPAddress.Any, port);
+                _listener.Start();
+                AppendChat("Đang lắng nghe ...");
+                lblStatus.Text = "Server đang chờ kết nối";
+                _ = AcceptClientAsync(_cts.Token);
+            }
+            catch (Exception ex)
+            {
+                AppendChat("Lỗi: " + ex.Message);
+                btnStart.Enabled = true;
+            }
+        }
+
+        private async Task AcceptClientAsync(CancellationToken token)
+        {
+            try
+            {
+                _client = await _listener.AcceptTcpClientAsync();
+                _stream = _client.GetStream();
+                UpdateStatus("Client đã kết nối");
+                StartReceiveLoop(token);
+            }
+            catch (ObjectDisposedException)
+            {
+                // Đã đóng
+            }
+            catch (Exception ex)
+            {
+                AppendChat("Lỗi: " + ex.Message);
+                SafeEnableStart();
+            }
+        }
+
+        private async Task ConnectAsClient(string ip, int port)
+        {
+            try
+            {
+                _client = new TcpClient();
+                await _client.ConnectAsync(IPAddress.Parse(ip), port);
+                _stream = _client.GetStream();
+                UpdateStatus("Đã kết nối server");
+                StartReceiveLoop(_cts.Token);
+            }
+            catch (Exception ex)
+            {
+                AppendChat("Không kết nối được: " + ex.Message);
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void StartReceiveLoop(CancellationToken token)
+        {
+            Task.Run(async () =>
+            {
+                var buffer = new byte[1024];
+                try
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        int len = await _stream.ReadAsync(buffer, 0, buffer.Length, token);
+                        if (len <= 0) break;
+                        string text = Encoding.UTF8.GetString(buffer, 0, len);
+                        AppendChat("Peer: " + text);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AppendChat("Ngắt kết nối: " + ex.Message);
+                }
+                finally
+                {
+                    UpdateStatus("Đã ngắt");
+                    SafeEnableStart();
+                }
+            }, token);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string msg = txtMessage.Text.Trim();
+            if (string.IsNullOrEmpty(msg)) return;
+
+            lock (_streamLock)
+            {
+                if (_stream == null || !_stream.CanWrite)
+                {
+                    AppendChat("Chưa sẵn sàng gửi.");
+                    return;
+                }
+
+                byte[] data = Encoding.UTF8.GetBytes(msg);
+                _stream.Write(data, 0, data.Length);
+            }
+
+            AppendChat("Me: " + msg);
+            txtMessage.Clear();
+        }
+
+        private void AppendChat(string text)
+        {
+            if (txtChat.InvokeRequired)
+            {
+                txtChat.Invoke(new Action<string>(AppendChat), text);
+                return;
+            }
+
+            txtChat.AppendText(text + Environment.NewLine);
+        }
+
+        private void UpdateStatus(string text)
+        {
+            if (lblStatus.InvokeRequired)
+            {
+                lblStatus.Invoke(new Action<string>(UpdateStatus), text);
+                return;
+            }
+            lblStatus.Text = text;
+        }
+
+        private void SafeEnableStart()
+        {
+            if (btnStart.InvokeRequired)
+            {
+                btnStart.Invoke(new Action(SafeEnableStart));
+                return;
+            }
+            btnStart.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _cts.Cancel();
+            try { _stream?.Close(); } catch { }
+            try { _client?.Close(); } catch { }
+            try { _listener?.Stop(); } catch { }
+        }
+    }
+}
