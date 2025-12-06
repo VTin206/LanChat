6cc47d8638d870ac05f047dfbc0b7b2be0224e62
LcChinh21 <leducchinhdz@gmail.com>
2025-12-06 19:52:05 +0700

    ADD CONSOLELOG AND CLIENTSOCKET + DISPLAY LOG + DISPLAY MESSAGE DEMO

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,10 +36,10 @@ namespace BasicChat
             this.txtIp = new System.Windows.Forms.TextBox();
             this.lblIp = new System.Windows.Forms.Label();
             this.rdClient = new System.Windows.Forms.RadioButton();
-            this.rdServer = new System.Windows.Forms.RadioButton();
             this.txtChat = new System.Windows.Forms.TextBox();
             this.txtMessage = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
+            this.rdServer = new System.Windows.Forms.RadioButton();
             this.grpConnection.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,9 +54,9 @@ namespace BasicChat
             this.grpConnection.Controls.Add(this.rdClient);
             this.grpConnection.Controls.Add(this.rdServer);
             this.grpConnection.Location = new System.Drawing.Point(16, 15);
-            this.grpConnection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpConnection.Margin = new System.Windows.Forms.Padding(4);
             this.grpConnection.Name = "grpConnection";
-            this.grpConnection.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpConnection.Padding = new System.Windows.Forms.Padding(4);
             this.grpConnection.Size = new System.Drawing.Size(611, 121);
             this.grpConnection.TabIndex = 0;
             this.grpConnection.TabStop = false;
@@ -76,7 +76,7 @@ namespace BasicChat
             // btnStart
             // 
             this.btnStart.Location = new System.Drawing.Point(473, 27);
-            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(115, 68);
             this.btnStart.TabIndex = 6;
@@ -87,7 +87,7 @@ namespace BasicChat
             // txtPort
             // 
             this.txtPort.Location = new System.Drawing.Point(340, 65);
-            this.txtPort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4);
             this.txtPort.Name = "txtPort";
             this.txtPort.Size = new System.Drawing.Size(105, 22);
             this.txtPort.TabIndex = 5;
@@ -106,7 +106,7 @@ namespace BasicChat
             // txtIp
             // 
             this.txtIp.Location = new System.Drawing.Point(340, 33);
-            this.txtIp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIp.Margin = new System.Windows.Forms.Padding(4);
             this.txtIp.Name = "txtIp";
             this.txtIp.Size = new System.Drawing.Size(105, 22);
             this.txtIp.TabIndex = 3;
@@ -126,30 +126,17 @@ namespace BasicChat
             // 
             this.rdClient.AutoSize = true;
             this.rdClient.Location = new System.Drawing.Point(129, 33);
-            this.rdClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdClient.Margin = new System.Windows.Forms.Padding(4);
             this.rdClient.Name = "rdClient";
             this.rdClient.Size = new System.Drawing.Size(61, 20);
             this.rdClient.TabIndex = 1;
             this.rdClient.Text = "Client";
             this.rdClient.UseVisualStyleBackColor = true;
             // 
-            // rdServer
-            // 
-            this.rdServer.AutoSize = true;
-            this.rdServer.Checked = true;
-            this.rdServer.Location = new System.Drawing.Point(25, 33);
-            this.rdServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.rdServer.Name = "rdServer";
-            this.rdServer.Size = new System.Drawing.Size(68, 20);
-            this.rdServer.TabIndex = 0;
-            this.rdServer.TabStop = true;
-            this.rdServer.Text = "Server";
-            this.rdServer.UseVisualStyleBackColor = true;
-            // 
             // txtChat
             // 
             this.txtChat.Location = new System.Drawing.Point(16, 143);
-            this.txtChat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtChat.Margin = new System.Windows.Forms.Padding(4);
             this.txtChat.Multiline = true;
             this.txtChat.Name = "txtChat";
             this.txtChat.ReadOnly = true;
@@ -160,7 +147,7 @@ namespace BasicChat
             // txtMessage
             // 
             this.txtMessage.Location = new System.Drawing.Point(16, 422);
-            this.txtMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4);
             this.txtMessage.Name = "txtMessage";
             this.txtMessage.Size = new System.Drawing.Size(488, 22);
             this.txtMessage.TabIndex = 2;
@@ -168,13 +155,26 @@ namespace BasicChat
             // btnSend
             // 
             this.btnSend.Location = new System.Drawing.Point(513, 420);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(113, 28);
             this.btnSend.TabIndex = 3;
             this.btnSend.Text = "Gửi";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rdServer
+            // 
+            this.rdServer.AutoSize = true;
+            this.rdServer.Checked = true;
+            this.rdServer.Location = new System.Drawing.Point(25, 33);
+            this.rdServer.Margin = new System.Windows.Forms.Padding(4);
+            this.rdServer.Name = "rdServer";
+            this.rdServer.Size = new System.Drawing.Size(68, 20);
+            this.rdServer.TabIndex = 0;
+            this.rdServer.TabStop = true;
+            this.rdServer.Text = "Server";
+            this.rdServer.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -187,7 +187,7 @@ namespace BasicChat
             this.Controls.Add(this.txtChat);
             this.Controls.Add(this.grpConnection);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -204,7 +204,6 @@ namespace BasicChat
 
         private System.Windows.Forms.GroupBox grpConnection;
         private System.Windows.Forms.RadioButton rdClient;
-        private System.Windows.Forms.RadioButton rdServer;
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.Label lblPort;
         private System.Windows.Forms.TextBox txtIp;
@@ -214,5 +213,6 @@ namespace BasicChat
         private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.RadioButton rdServer;
     }
 }
---
+++ Networking/ClientSocket.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BasicChat.Networking
+{
+    internal class ClientSocket
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread listenThread;
+
+        public string NickName { get; private set; }
+        public bool IsConnected { get; private set; } = false;
+
+        // Gửi event về UI
+        public event Action<Message> OnMessageReceived;
+        public event Action<string> OnDisconnected;
+
+        // ================================
+        // KẾT NỐI TỚI SERVER
+        // ================================
+        public void Connect(string ip, int port, string nickname)
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ip, port);
+
+                stream = client.GetStream();
+                IsConnected = true;
+
+                NickName = nickname;
+
+                // Gửi JSON nickname
+                var hello = Message.System($"{nickname}");
+                SendMessage(hello);
+
+                // Bắt đầu lắng nghe
+                listenThread = new Thread(ListenForMessages);
+                listenThread.IsBackground = true;
+                listenThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to connect: {ex.Message}");
+            }
+        }
+
+        // ================================
+        // GỬI MESSAGE JSON LÊN SERVER
+        // ================================
+        public void SendMessage(Message msg)
+        {
+            if (!IsConnected) return;
+            if (msg == null) return;
+
+            string json = msg.ToJson();
+            byte[] buffer = Encoding.UTF8.GetBytes(json);
+
+            try
+            {
+                stream.Write(buffer, 0, buffer.Length);
+            }
+            catch
+            {
+                Disconnect();
+            }
+        }
+
+        // ================================
+        // LUỒNG LẮNG NGHE SERVER
+        // ================================
+        private void ListenForMessages()
+        {
+            byte[] buffer = new byte[4096];
+
+            while (IsConnected)
+            {
+                try
+                {
+                    int byteCount = stream.Read(buffer, 0, buffer.Length);
+
+                    if (byteCount == 0)
+                    {
+                        Disconnect();
+                        return;
+                    }
+
+                    string json = Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+                    Message msg = Message.FromJson(json);
+                    OnMessageReceived?.Invoke(msg);
+                }
+                catch
+                {
+                    Disconnect();
+                    return;
+                }
+            }
+        }
+
+        // ================================
+        // NGẮT KẾT NỐI
+        // ================================
+        public void Disconnect()
+        {
+            if (!IsConnected) return;
+
+            IsConnected = false;
+
+            try { stream?.Close(); } catch { }
+            try { client?.Close(); } catch { }
+
+            OnDisconnected?.Invoke("Disconnected from server.");
+
+            try { listenThread?.Join(); } catch { }
+        }
+    }
+}
